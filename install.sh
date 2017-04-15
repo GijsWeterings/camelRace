41ebfa906965e3adb09cf0c199703d2629babff1
Gijs Weterings <gijsweterings+github@gmail.com>
2017-04-15 18:11:14 +0200

    install wiringpi via pip, run install python script

--- install.sh
+++ install.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
+sudo apt-get update
 sudo apt-get install -y git python-dev python-setuptools swig
 
 # Download the latest version of wiringPi
-git clone --recursive https://github.com/WiringPi/WiringPi-Python.git wiringPi
-
-# Build the WiringPi dependency
-cd wiringPi
-git pull origin
-./build.sh
-cd ..
+pip install wiringpi2
 
 # Test if WiringPi is installed succesfully
-hash gpio -v 2>/dev/null || { echo >&2 "WiringPi was not installed succesfully. Run gpio -v for more information. Aborting."; exit 1; }
-
+python scripts/testinstall.py
---
+++ scripts/testinstall.py
@@ -0,0 +1,3 @@
+import wiringpi
+
+wiringpi.piBoardRev()
