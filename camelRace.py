475cb750ecac2e4e4adbedc8cf548c7b86bf1ea7
Gijs Weterings <gijsweterings+github@gmail.com>
2017-06-13 09:32:07 +0200

    Camelrace implementation

--- camelRace.py
+++ camelRace.py
@@ -4,7 +4,9 @@ from kivy.config import Config
 kivy.require('1.9.1')
 
 Config.set('kivy', 'exit_on_escape', 1)
-Config.set('graphics', 'fullscreen', 1)
+Config.set('graphics', 'fullscreen', 0)
+Config.set('graphics', 'width', '1920')
+Config.set('graphics', 'height', '1080')
 Config.write()
 
 from kivy.app import App
@@ -12,51 +14,85 @@ from kivy.uix.label import Label
 from kivy.uix.scatter import Scatter
 from kivy.properties import StringProperty
 from kivy.uix.floatlayout import FloatLayout
+from kivy.core.window import Window
 
 class Picture(Scatter):
-    '''Picture is the class that will show the image with a white border and a
-    shadow. They are nothing here because almost everything is inside the
-    picture.kv. Check the rule named <Picture> inside the file, and you'll see
-    how the Picture() is really constructed and used.
-    The source property will be the filename to show.
-    '''
-
     source = StringProperty(None)
 
-
 class CamelRaceApp(App):
+
+    pinsToSense = [[8,9,7],[0,2,3],[12,13,14]]
+
+    def __init__(self, **kwargs):
+        super(CamelRaceApp, self).__init__(**kwargs)
+        self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
+        self._keyboard.bind(on_key_down=self._on_keyboard_down)
+        self.gameOver = False
+
+        for idx, pinList in enumerate(self.pinsToSense):
+            player = self.getPlayers()[idx]
+            for pinIdx, pin in enumerate(pinList):
+                wiringpi.pinMode(pin, wiringpi.GPIO.INPUT)
+                wiringpi.pullUpDnControl(pin, wiringpi.GPIO.PUD_UP)
+                wiringpi.wiringPiISR(pin, wiringpi.GPIO.INT_EDGE_BOTH, lambda x: self.increment(player, 10 * pinIdx))
+
+
 
     def build(self):
         root = self.root #FloatLayout()
         print root.width
         print root.height
         
-        player1 = Picture(source="images/red.png", pos=(root.width, root.height / 2))
-        player2 = Picture(source="images/blue.png", pos=(root.width - 200, root.height / 2))
-        player3 = Picture(source="images/green.png", pos=(root.width - 300, root.height / 2))
+        self.player1 = Picture(source="images/red.png", pos=(root.width, 700))
+        self.player2 = Picture(source="images/blue.png", pos=(root.width, 400))
+        self.player3 = Picture(source="images/green.png", pos=(root.width, 100))
 
         # add to the main field
-        root.add_widget(player1)
-        root.add_widget(player2)
-        root.add_widget(player3)
+        root.add_widget(self.player1)
+        root.add_widget(self.player2)
+        root.add_widget(self.player3)
+
+
+    def _keyboard_closed(self):
+        self._keyboard.unbind(on_key_down=self._on_keyboard_down)
+        self._keyboard = None
+
+    def showWinner(self, player):
+        self.gameOver = True
+        player.center_x = 1000.0
+        player.center_y = 500
+        player.scale = 3
+
+    def increment(self, player, amount = 10):
+        player.center_x += amount
+        if player.center_x >= 1820 and not self.gameOver:
+            self.showWinner(player)
+
+    def decrement(self, player, amount= 10):
+        player.center_x -= amount
+
+    def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
+        if self.gameOver:
+            return False
+        if keycode[1] == '1':
+            self.increment(self.player1)
+        elif keycode[1] == '2':
+            self.increment(self.player2)
+        elif keycode[1] == '3':
+            self.increment(self.player3)
+        elif keycode[1] == 'q':
+            self.decrement(self.player1)
+        elif keycode[1] == 'w':
+            self.decrement(self.player2)
+        elif keycode[1] == 'e':
+            self.decrement(self.player3)
+        return True
+
+
 
 
 if __name__ == '__main__':
-    CamelRaceApp().run()
-    PIN_TO_SENSE = 23
-
     ## Start WiringPi with the WiringPi pin setup
     ## May have to switch to GPIO setup
-    # wiringpi.wiringPiSetup()
-
-    # def gpio_callback():
-    #     print "GPIO_CALLBACK!"
-
-    # wiringpi.pinMode(PIN_TO_SENSE, wiringpi.GPIO.INPUT)
-    # wiringpi.pullUpDnControl(PIN_TO_SENSE, wiringpi.GPIO.PUD_UP)
-
-    # wiringpi.wiringPiISR(PIN_TO_SENSE, wiringpi.GPIO.INT_EDGE_BOTH, gpio_callback)
-
-    # Keep the program running
-    #while True:
-        # wiringpi.delay(2000)
+    wiringpi.wiringPiSetup()
+    CamelRaceApp().run()
